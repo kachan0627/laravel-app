baded0836bb7022dd79da09c5c6a4966982090cd
Katsu <kachan.0627.619@gmail.com>
2020-04-13 11:19:46 +0900

    4/13-02 commit

--- tests/Feature/UserTest.php
+++ tests/Feature/UserTest.php
@@ -5,18 +5,36 @@ namespace Tests\Feature;
 use Tests\TestCase;
 use Illuminate\Foundation\Testing\WithFaker;
 use Illuminate\Foundation\Testing\RefreshDatabase;
+use Illuminate\Foundation\Testing\DatabaseTransactions;
+use App\Models\User;
+
 
 class UserTest extends TestCase
 {
+  use DatabaseTransactions;
     /**
      * A basic feature test example.
      *
      * @return void
      */
-    public function testExample()
+
+  /*  public function testExample()
     {
         $response = $this->get('/');
 
         $response->assertStatus(200);
+        //$this->assertTrue(true);
+    }*/
+   public function testUserLogin()
+    {
+      //偽のユーザを作成
+      $user = factory(User::class,'student')->create();
+      //ログイン処理
+      $this->visit('/login')
+           ->type($user->email,'email')
+           ->type('secret','password')
+           ->press('ログイン')
+           ->seePageIs('/home');
+
     }
 }
--- tests/TestCase.php
+++ tests/TestCase.php
@@ -7,4 +7,6 @@ use Illuminate\Foundation\Testing\TestCase as BaseTestCase;
 abstract class TestCase extends BaseTestCase
 {
     use CreatesApplication;
+    //test中でもlocal環境を見に行ってしまうため、キャッシュを読み込まないようにしている
+    //if(file_exists(_DIR_.''))
 }
