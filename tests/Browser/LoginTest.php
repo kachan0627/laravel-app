47aa2aecaba6fc0921ddd8e0bad1a8efdc8c6468
Katsu <kachan.0627.619@gmail.com>
2020-04-21 14:06:49 +0900

    4/21ログインページのブラウザテスト〜dusk〜完了

--- tests/Browser/LoginTest.php
+++ tests/Browser/LoginTest.php
@@ -5,6 +5,7 @@ namespace Tests\Browser;
 use Tests\DuskTestCase;
 use Laravel\Dusk\Browser;
 use Illuminate\Foundation\Testing\DatabaseMigrations;
+use App\Models\User;
 
 class LoginTest extends DuskTestCase
 {
@@ -13,11 +14,35 @@ class LoginTest extends DuskTestCase
      *
      * @return void
      */
-    public function testExample()
+
+     //ログインできない場合のテスト（passwordFalse）
+    public function testLoginPasswordFalse()
     {
-        $this->browse(function (Browser $browser) {
-            $browser->visit('/')
-                    ->assertSee('Laravel');
+
+      $this->browse(function (Browser $browser){
+          $browser->visit('/login')
+                  ->type('email','test3@test.com')
+                  ->type('password','1234567')
+                  ->press('Login')
+                  ->assertPathIsNOT('/home');
+
+
+      });
+    }
+     //ログインできる場合のテスト
+   public function testLoginTrue()
+    {
+        $this->browse(function (Browser $browser){
+            $browser->visit('/login')
+                    ->type('email','test3@test.com')
+                    ->type('password','12345678')
+                    ->press('Login')
+                    ->assertPathIs('/home')
+                    ->visit('/logout');
+
+
         });
     }
+
+
 }
